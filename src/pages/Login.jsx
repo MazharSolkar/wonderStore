776ba42adc8d0ce44de838ebcd0734ab7a1156c4
MazharSolkar <mazharsolkar65@gmail.com>
2023-10-01 13:00:44 +0530

    guest user login functionality done ✔

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,9 @@
 import { FormInput, SubmitBtn } from '../components';
-import { Form, Link } from 'react-router-dom';
+import { Form, Link, useNavigate, redirect } from 'react-router-dom';
 import { customFetch } from '../utils';
 import { toast } from 'react-toastify';
 import { loginUser } from '../features/user/userSlice';
 import { useDispatch } from 'react-redux';
-import { redirect } from 'react-router-dom';
 
 export const action = (store) => {
   return async ({ request }) => {
@@ -28,29 +27,40 @@ export const action = (store) => {
 };
 
 const Login = () => {
-  // console.log(something to check <ErrorElement />);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const loginAsGuestUser = async () => {
+    try {
+      const response = await customFetch.post('/auth/local', {
+        identifier: 'test@test.com',
+        password: 'secret',
+      });
+      dispatch(loginUser(response.data));
+      toast.success('Welcome guest user');
+      navigate('/');
+    } catch (error) {
+      console.log(error);
+      toast.error('guest user login error. please try again');
+    }
+  };
+
   return (
     <section className='h-screen grid place-content-center'>
+      {/* Hi there everyone */}
       <Form
         method='post'
         className='card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4'>
         <h4 className='text-center text-3xl font-bold'>Login</h4>
-        <FormInput
-          type='email'
-          label='email'
-          name='identifier'
-          defaultValue='test@test.com'
-        />
-        <FormInput
-          type='password'
-          label='password'
-          name='password'
-          defaultValue='secret'
-        />
+        <FormInput type='email' label='email' name='identifier' />
+        <FormInput type='password' label='password' name='password' />
         <div className='mt-4'>
           <SubmitBtn text='login' />
         </div>
-        <button className='btn btn-secondary btn-block' type='button'>
+        <button
+          className='btn btn-secondary btn-block'
+          type='button'
+          onClick={loginAsGuestUser}>
           guest user
         </button>
         <p className='text-center'>
